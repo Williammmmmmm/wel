5c3a5f1bf24d33bbe322ccdaf8ee3ebf2a43ade6
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-05-09 16:18:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,10 +13,10 @@ const p2 = new D1.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("573399652305731596");
+const fuli= member.guild.channels.get("569343219800735752");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to Spoil.**`), 4000)
+setTimeout(() => fuli.send(`**Hello it’s Blanc.`), 4000)
 }
 });
 
