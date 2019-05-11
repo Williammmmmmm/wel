5ce1c1dad9e086c77efc95758b7970a2635ab3c2
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-05-11 11:14:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,14 @@
-const D1 = require('discord.js'); 
-const D2 = require('discord.js');  
+const D1 = require('discord.js');  
 
 
 
-const p1 = new D1.Client();
-const p2 = new D1.Client();
-
+const p1 = new D2.Client();
 
 
 
 
 
-    console.log("oNNN1");
-p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("573399652305731596");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`** Welcome to Spoil. **`), 4000)
-}
-});
+
 
  
 
@@ -39,4 +29,3 @@ setTimeout(() => fuli.send(`**Welcome To Epsilon.**`), 4000)
 
 
 p1.login(process.env.B);
-p2.login(process.env.B2);
