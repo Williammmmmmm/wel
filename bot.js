ed6c9a1786996de521b970da10c6d23696f61403
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-05-11 11:15:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const D1 = require('discord.js');
 
 
 
-const p1 = new D2.Client();
+const p1 = new D1.Client();
 
 
 
@@ -14,8 +14,8 @@ const p1 = new D2.Client();
 
 
 
-    console.log("oNNN2");
-p2.on('guildMemberAdd', member => {
+    console.log("oNNN1");
+p1.on('guildMemberAdd', member => {
 const fuli= member.guild.channels.get("521045311213535262");
 if(!fuli) return;
 if(fuli) {
