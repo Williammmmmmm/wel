200c42a5f8cb732cf3ef3d829311169664a071eb
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-05-13 22:56:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,10 @@
 const D1 = require('discord.js');
 const D2 = require('discord.js');
-const D3 = require('discord.js');
-const D4 = require('discord.js');
-
-
 
 
 
 const p1 = new D1.Client();
 const p2 = new D2.Client();
-const p3 = new D3.Client();
-const p4 = new D4.Client();
-
 
 
 
@@ -31,29 +24,10 @@ p2.on('guildMemberAdd', member => {
 const fuli= member.guild.channels.get("564191993018974209");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 7800)
+setTimeout(() => fuli.send(`**Welcome To Epsilon.**`), 7800)
 }
 });
 
-   console.log("oNNN3");
-p3.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("564191993018974209");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 9000)
-}
-});
-
-
-
-   console.log("oNNN4");
-p4.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("521045311213535262");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`**Welcome To  Epsilon.**`), 6000)
-}
-});
 
 
 
@@ -66,5 +40,3 @@ setTimeout(() => fuli.send(`**Welcome To  Epsilon.**`), 6000)
 
 p1.login(process.env.B);
 p2.login(process.env.B2);
-p3.login(process.env.B3);
-p4.login(process.env.B4);
