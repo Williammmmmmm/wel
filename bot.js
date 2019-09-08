64a84b448b3988088bf423f9e62e96fa5ef30370
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-09-08 18:06:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const D1 = require('discord.js');
-const D2 = require('discord.js');
+
 
 
 
 
 
 const p1 = new D1.Client();
-const p2 = new D2.Client();
+
 
 
 
@@ -15,21 +15,13 @@ const p2 = new D2.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("577536069692817418");
+const fuli= member.guild.channels.get("619479070312235019");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to Bùbbles.**`), 8000)
+setTimeout(() => fuli.send(`You're in **Valor** Welcome.`), 8000)
 }
 });
 
-   console.log("oNNN2");
-p2.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("521045311213535262");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`**Welcome to  Epsilon.**`), 7800)
-}
-});
 
   
 
@@ -47,4 +39,3 @@ setTimeout(() => fuli.send(`**Welcome to  Epsilon.**`), 7800)
 
 
 p1.login(process.env.B);
-p2.login(process.env.B2);
