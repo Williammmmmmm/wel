3c5080e3f5d51bf4662cf0866a61295443dd0be8
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-09-09 19:10:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ p1.on('guildMemberAdd', member => {
 const fuli= member.guild.channels.get("619479070312235019");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`You're in **Valor** Welcome.`), 8000)
+setTimeout(() => fuli.send(`You're in **Valor** Welcome.`), 40000)
 }
 });
 
