7b2580045a35fee174a8ed54094acf6b0d77391a
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-09-13 17:15:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const p1 = new D1.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("619479070312235019");
+const fuli= member.guild.channels.get("621423241608822804");
 if(!fuli) return;
 if(fuli) {
 setTimeout(() => fuli.send(`Hello it's Violin ...`), 40000)
