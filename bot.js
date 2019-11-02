95134e65571084fb36e0efc4346ee64bffee0442
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2019-11-02 20:09:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const D1 = require('discord.js');
-const D2 = require('discord.js');
 
 
 
@@ -7,7 +6,7 @@ const D2 = require('discord.js');
 
 
 const p1 = new D1.Client();
-const p2 = new D2.Client();
+
 
 
 
@@ -17,23 +16,14 @@ const p2 = new D2.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("629268891700428810");
+const fuli= member.guild.channels.get("639899318580674581");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`*Hello , you're in dint.*`), 10000)
+setTimeout(() => fuli.send(`You're in **Serenity.**`), 10000)
 }
 });
 
 
-  
-    console.log("oNNN2");
-p2.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("629268891700428810");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`*Hello , you're in dint.*`), 10000)
-}
-});
 
 
 
@@ -49,5 +39,4 @@ setTimeout(() => fuli.send(`*Hello , you're in dint.*`), 10000)
 
 
 p1.login(process.env.B);
-p2.login(process.env.B2);
 
