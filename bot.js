50ecde1f2fa4e97c812a946a79fde67c750659b8
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-13 10:32:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const p1 = new D1.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("676525568791019521");
+const fuli= member.guild.channels.get("645729148429402112");
 if(!fuli) return;
 if(fuli) {
 setTimeout(() => fuli.send(`**i don't wanna be you anymore**`), 2000)
