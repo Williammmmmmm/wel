cb3d7dd4ec24ca42b712ee9ad104030c09a24771
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-13 07:40:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const p1 = new D1.Client();
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("639899318580674581");
+const fuli= member.guild.channels.get("655465179018166275");
 if(!fuli) return;
 if(fuli) {
 setTimeout(() => fuli.send(`greetings our new pùrt; companion..**`), 2000)
