9045e59c3e0cba1f3436c181f26957af70863e1b
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-19 20:54:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,41 @@
 const D1 = require('discord.js');
+
+
+
+
+
+
 const p1 = new D1.Client();
-console.info("oNNN1");
+
+
+
+
+
+
+
+
+    console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
 const fuli= member.guild.channels.get("683035809858977927");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send("ترحيب"), 12000)
+setTimeout(() => fuli.send(# - Wélcome Tø Hørizon ,,), 1500)
 }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 p1.login(process.env.B);
