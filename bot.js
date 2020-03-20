51412cc0d5ca2f25becd26c71bfba1a7a1926b50
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-20 23:36:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,12 @@
-const D1 = require('discord.js');
-
-
-
-
-
-
-const p1 = new D1.Client();
-
-
-
-
-
-
-
-
-    console.log("oNNN1");
-p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("683035809858977927");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => VVilly.send(`**ترحيب**`), 15000)}
+const Discord = require('discord.js'),
+Willy = new Discord.Client({sisableEveryone: true})
+Willy.on('guildMemberAdd', member => {
+console.log("Welcoming..");
+const VVilly= member.guild.channels.get("573399652305731596");
+if(!VVilly) return;
+if(VVilly) {
+setTimeout(() => VVilly.send(`ترحيب`), 15000)
+}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 p1.login(process.env.B);
