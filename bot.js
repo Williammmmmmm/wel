4eddbb5544ac19e1d35898c1e382145b6cee739c
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-25 15:41:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,11 @@
-const D1 = require('discord.js');
-
-
-
-
-
-
-const p1 = new D1.Client();
-
-
-
-
-
-
-
-
-    console.log("oNNN1");
-p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("676525568791019521");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`# - Wélcome Tø Elite), 2000)
+const Discord = require('discord.js'),
+Willy = new Discord.Client({sisableEveryone: true})
+Willy.on('guildMemberAdd', member => {
+console.log("Welcoming..");
+const VVilly= member.guild.channels.get("676525568791019521");
+if(!VVilly) return;
+if(VVilly) {
+setTimeout(() => VVilly.send(`**.Welcome to Elite **`), 1500)
 }
 });
 
