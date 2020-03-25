6dc918cc55bb0b9a63fcce657bf2353ce786de50
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-25 20:22:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-client.on("guildMemberAdd", member => {
+const Discord = require('discord.js'),
  const welcomer =  member.guild.channels.find('683035809858977927');
 if(!welcomer) return;
 welcomer.send(`> **# - Wélcome Tø Hørizon ,,**`);});
