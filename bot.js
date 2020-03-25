104793e35d8ff68d3e8fe10b6e6bf0b38887a9df
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-25 16:44:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "*"
 
+
+client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
-    console.log("oNNN1");
+
 p1.on('guildMemberAdd', member => {
 const fuli= member.guild.channels.get("683035809858977927");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`> **# - Wélcome Tø Hørizon ,,**`), 2000)
+setTimeout(() => fuli.send(> **# - Wélcome Tø Hørizon ,,**), 2000)
 }
 });
 
 client.login(process.env.BOT_TOKEN);
+
