641bc5c488cbcb8158ecc6b6e7ae2fc034e0b4b3
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-26 17:52:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ bot.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("683035809858977927");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`> **# - Wélcome Tø Hørizon ,,**`), 9000)        
+setTimeout(() => mohamed.send(`ترحيب`), 9000)        
 }
 });
 
