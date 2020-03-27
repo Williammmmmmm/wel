94b06cad59a1e6e392b60a942f165c8a096d6177
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-27 15:03:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,8 @@
-const Discord = require('discord.js'),
-    bot = new Discord.Client({sisableEveryone: true})
-    console.log("gg");
-bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("683035809858977927");
-if(!mohamed) return;
-if(mohamed) {
-setTimeout(() => mohamed.send(`> **# - Wélcome Tø Hørizon ,,**`), 9000)        
+client.on('guildMemberAdd', member => {
+const William = member.guild.channels.get("692768245224308810");
+if(!William) return;
+if(William) {
+setTimeout(() => William.send("> **# - Wélcome Tø Hørizon ,,**"), 3000)
 }
 });
 
