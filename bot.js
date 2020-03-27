7e51d803dc42faae649d2770f8ea2e7605356e9a
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-03-27 16:59:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,5 @@
-client.on('guildMemberAdd', member => {
-const William = member.guild.channels.get("693068099745742933");
-if(!William) return;
-if(William) {
-setTimeout(() => William.send("-"), 3000)
-}
+client.on("guildMemberAdd", member => { 
+    client.channels.cache.get('693068099745742933').send(`qeqeqeqeqeqee`); 
 });
 
-client.on('guildMemberAdd', member => {
-const William = member.guild.channels.get("693068099745742933");
-if(!William) return;
-if(William) {
-setTimeout(() => William.send("> **# - Wélcome Tø Hørizon ,,**"), 5000)
-}
-});
-
+bot.login(process.env.BOT_TOKEN);
