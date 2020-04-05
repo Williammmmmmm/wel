34568d10ce3a194d73d60f3817791238bd735fe6
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-04-05 15:24:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
-const D1 = require('discord.js');
-const p1 = new D1.Client();
-console.info("oNNN1");
-p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("696311055277752363");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send("# - Wélcome Tø Hørizon ,,"), 2000)
+ const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("621839367463501855");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`*You’re in **Riiich** , Welcome.*`), 4000)        
 }
 });
-bot.login(process.env.BOT_TOKEN);//
+
+bot.login(process.env.BOT_TOKEN);// 
