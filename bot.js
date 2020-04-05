33de3b76e9396568812d3898ebe8e5c1a0585439
Williammmmmmm <50382704+Williammmmmmm@users.noreply.github.com>
2020-04-05 15:36:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,33 @@
- const Discord = require('discord.js'),
-    bot = new Discord.Client({sisableEveryone: true})
-    console.log("muuuuuuuuute,");
-bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("621839367463501855");
-if(!mohamed) return;
-if(mohamed) {
-setTimeout(() => mohamed.send(`*You’re in **Riiich** , Welcome.*`), 4000)        
-}
-});
+const {
+  tokens,
+  voice_channel,
+  text_channel,
+  your_server
+} = require("./config.js");
+const time = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000];
+const Discord = require("discord.js");
+tokens.forEach(token => {
+  const client = new Discord.Client();
+  client.login(token);
 
-bot.login(process.env.BOT_TOKEN);// 
+  client.on("message", async message => {
+    if (message.guild.id != your_server) return;
+    if (
+      message.author.id == "323987823139094529" && // تعديل مهم ايدي بوت الترحيب
+      message.content.includes("-") /// (الاستقبال) تعديل مهم رسالة الترحيب
+    ) {
+      setTimeout(() => {
+        message.channel.send("*You’re in **Riiich** , Welcome.*"); ///  (الارسال)تعديل مهم رسالة الترحيب
+      }, time[Math.floor(Math.random() * time.length)]);
+    }
+  });
+
+  client.on("ready", () => {
+    console.log(client.user.tag + " is ready!");
+    client.guilds
+      .get(your_server)
+      .channels.get(voice_channel)
+      .join();
+  });
+
+});
